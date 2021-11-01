9dc6693df8c6c8a32a58e73cd28d4c2f6aa4e2d0
luisfesu <luisfesu2000@gmail.com>
2021-11-01 01:29:11 +0100

    me quedo sin ideas para los commits, simplemente he hecho demasiado

--- XML_2_KML.py
+++ XML_2_KML.py
@@ -8,18 +8,19 @@ from typing import Text
 import xml.etree.ElementTree as ET
 
 # Variable Global file
-file = open("coordenadas_familia.kml", "w") # Abre y borra automaticamente el contenido del archivo
+file = open("coordenadasFamilia.kml", "w") # Abre y borra automaticamente el contenido del archivo
 
 def write_in_file(line):
     """ Funcion que escribe la linea en el archivo y agrega un salto de linea"""
     file.write(line)
     file.write("\n")
 
+
 def xml_2_kml(archivo_XML):
     """
     Función xml_2_kml(archivo_XML):
     Transforma el contenido de un archivo XML basado
-    en el XML Schema arbolGenealogico en un documento HTML
+    en el XML Schema arbolGenealogico en un documento KML que pueda ser interpretado por Google Earth
 
     Author: Luis Antonio Fernandez Suarez
     """
@@ -37,27 +38,34 @@ def xml_2_kml(archivo_XML):
     write_in_file_kml_head()
 
     write_in_file('<Document>')
-    write_name_tag('coordenadas_familia')
+    write_name_tag('coordenadas de familiares')
     
-
     familiar_raiz = raiz.find("familiar[1]")
     parse_coordenadas_familiar(familiar_raiz) # Adaptar elemento <Familiar> de XML a elemento <Placemark> de KML
 
     write_in_file('</Document>')
+    write_in_file('</kml>')
+
 
 def parse_coordenadas_familiar(familiar):
     ''' Funcion que adapta un elemento <Familiar> de XML Schema 'arbolGenealogico.xsd' a elemento <Placemark> de KML'''    
     data = familiar.find("./dataFamiliar[1]") # consigue el elemento <dataFamiliar>
 
-    write_in_file("<placemark>")
-
     nacimiento = data.find("./nacimiento")
     parse_lugar_nacimiento(nacimiento, familiar)
 
+    fallecimiento = data.find("./fallecimiento")
+    if fallecimiento != None: # Añade un placemark para el lugar de muerte en caso de haberlo
+        parse_lugar_fallecimiento(fallecimiento,familiar)
 
-    write_in_file("</placemark>")
+    for parent in familiar.findall('./familiar'): # Padres del familiar
+        parse_coordenadas_familiar(parent) # Llamada recursiva que recorre todo el arbol de "arbolGenealogico.xml"
+
 
 def parse_lugar_nacimiento(nacimiento, familiar):
+    '''
+        Añade un elemento <Placemark> para la ubicacion de nacimiento de un familiar
+    '''
     nombre = familiar.attrib.get('nombre')
     apellido = familiar.attrib.get('apellido')
     lugar = nacimiento.find('./lugar').text
@@ -73,15 +81,44 @@ def parse_lugar_nacimiento(nacimiento, familiar):
     write_name_tag('Lugar de nacimiento de ' + nombre + ' ' + apellido) # Nombre de la placemark
 
     write_in_file('<Point>')
-
     write_coordinates_tag('{0},{1},{2}'.format(str(longitud), str(latitud), str(altitud))) # String with Format: <long>,<lat>,<alt>
-
     write_in_file('</Point>')
+
+    write_in_file('</Placemark>')
+
+
+def parse_lugar_fallecimiento(fallecimiento, familiar):
+    '''
+        Añade un elemento <Placemark> para la ubicacion de fallecimiento de un familiar
+    '''
+    nombre = familiar.attrib.get('nombre')
+    apellido = familiar.attrib.get('apellido')
+    lugar = fallecimiento.find('./lugar').text
+
+    coordenadas = fallecimiento.find('./coordenadas') # Elemento <Coordenadas> perteneciente a XML Schema "arbolGenealogico.xsd"
+
+    longitud = coordenadas.attrib.get('longitud')
+    latitud = coordenadas.attrib.get('latitud')
+    altitud = coordenadas.attrib.get('altitud')
+
+    write_in_file('<Placemark>') # Placemark de fallecimiento
+
+    write_name_tag('Lugar de fallecimiento de ' + nombre + ' ' + apellido) # Nombre de la placemark
+
+    write_in_file('<Point>')
+    write_coordinates_tag('{0},{1},{2}'.format(str(longitud), str(latitud), str(altitud))) # String with Format: <long>,<lat>,<alt>
+    write_in_file('</Point>')
+    
     write_in_file('</Placemark>')
 
 
 def write_in_file_kml_head():
-    write_in_file("<?xml version="1.0" encoding="UTF-8"?>")
+    '''
+    Incluye la cabecera del archivo kml:
+        <?xml version="1.0" encoding="UTF-8"?>
+        <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+    '''
+    write_in_file('<?xml version="1.0" encoding="UTF-8"?>')
     write_in_file('<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">')
 
 
@@ -92,12 +129,14 @@ def write_name_tag(name):
     '''
     write_in_file('<name> ' + name + ' </name>')
 
+
 def write_coordinates_tag(coordinates):
     '''
     Incluye un elemento <coordinates>  con el siguiente formato
         <coordinates> 'Coordenadas separadas por comas dadas como parametro' </coordinates>
     '''
     write_in_file('<coordinates>' + coordinates + '</coordinates>')
+
 
 def main():
     print(xml_2_kml.__doc__)
@@ -107,6 +146,7 @@ def main():
 
     file.close()
     print
+
 
 if __name__ == "__main__":
     main()
---
+++ XML_2_SVG.py
@@ -0,0 +1,196 @@
+# # -*- coding: utf-8 -*-
+""""
+Trasformar Codigo XML a KML usando XPath
+@author: Luis Antonio Fernandez Suarez
+"""
+
+from typing import Text
+import xml.etree.ElementTree as ET
+
+# Variable Global file
+file = open("arbolFamiliar.svg", "w") # Abre y borra automaticamente el contenido del archivo
+
+# Variable global y final que nos indica el tamaño de cada fila en el arbol
+ROW_HEIGHT = 100
+
+# Variable global y final que nos indica el tamaño de cada caja de familiar en el arbol
+BOX_HEIGHT = 80
+BOX_WIDTH = 200
+
+# Variable Global que nos indica en que fila estamos trabajando,
+# cada vez que añadamos un familiar, esta variable aumentara en uno.
+current_row = 1
+
+# Variable Global que nos indica si un familiar es l raiz del arbol
+# pasará a ser negativa en la primera iteraccion de xml_2_svg(archivo_XML)
+root = True
+
+def write_in_file(line):
+    """ Funcion que escribe la linea en el archivo y agrega un salto de linea"""
+    file.write(line)
+    file.write("\n")
+
+
+def xml_2_svg(archivo_XML):
+    """
+    Función xml_2_svg(archivo_XML):
+    Transforma el contenido de un archivo XML basado
+    en el XML Schema arbolGenealogico en un documento SVG
+
+    Author: Luis Antonio Fernandez Suarez
+    """
+    try:
+        arbol = ET.parse(archivo_XML)
+    except IOError:
+        print("No se encuentra el archivo ", archivo_XML)
+        exit()
+
+    except ET.ET.ParseError:
+        print("Error procesando el archivo XML ", archivo_XML)
+        exit()
+    
+    raiz = arbol.getroot() # Raiz del documento
+    write_in_file_svg_head()
+
+    familiar_raiz = raiz.find("familiar[1]")
+    
+    # Coordenadas de origen situadas fuera del area visible que permiten crear la raiz del 
+    # documento en el punto inicial (10,10)
+    x = -190
+    y = 10
+
+    draw_box_for_familiar(familiar_raiz,x,y)
+    write_in_file("</svg>")
+
+
+def draw_box_for_familiar(familiar,x_origen,y_origen):
+    """
+    Dibuja el contenido del elemento familiar en una caja
+    cuyas coordenadas dependeran de la caja de origen (x_origen,y_origen) y de la fila actual
+    """
+    global current_row # Para indicarle a la funcion que current_row hace referencia a la variable global 
+    global root # Para indicarle a la funcion que root hace referencia a la variable global 
+
+    x = x_origen + 200
+    
+    if root is True: # if state que sirve para crear el nodo raiz en las coordenadas (10,10)
+        y = 10
+    else:
+        y = current_row * ROW_HEIGHT
+
+    box = '<rect x="{0}" y="{1}" width="{2}" height="{3}" style="fill:white;stroke:black;stroke-width:1" />'.format(
+        str(x), str(y), str(BOX_WIDTH), str(BOX_HEIGHT)
+    )
+    write_in_file(box) # Incluye  la caja creada para este familiar
+
+    write_familiar_data(familiar, x, y) # Incluye los datos personales
+
+    if root is False: # La raiz no tiene ningun path de origen
+        draw_path(x_origen, x,y_origen, y) # dibujar la union entre esta caja y su caja padre (x_origen, y_origen)
+    else:
+        root = False # Despues de pasar este if-statement por primera vez dejamos de estar en la raiz
+
+    current_row = current_row + 1 # IMPORTANTE: Incrementa la fila para que el siguiente familiar se pinte en la linea siguiente
+
+    for parent in familiar.findall("./familiar"):
+        draw_box_for_familiar(parent,x,y) # Llamada recursiva para crear sus hijos
+
+
+
+def draw_path(x1, x2, y1, y2):
+    """
+    Dibuja una linea quebrada que va desde el vertice inferior de una caja de origen (x1,y1) 
+    hasta el vertice izquierdo de una caja de destino (x2,y2)
+    """
+    x_origen = x1 + (BOX_WIDTH / 2)
+    y_origen = y1 + BOX_HEIGHT
+
+    desplazamiento_vertical = (y2 + BOX_HEIGHT / 2) - y_origen # vector de desplazamiento vertical
+    desplazamiento_horizontal = x2 - x_origen # vector de desplazamiento horizontal
+
+    path = "M {0} {1} v {2} h {3}".format(
+        str(x_origen), str(y_origen),
+        str(desplazamiento_vertical),
+        str(desplazamiento_horizontal)
+    )
+
+    write_in_file('<path d="{0}" style="fill:transparent;stroke:black" />'.format(path))
+
+
+def write_familiar_data(familiar,x,y):
+    x = x + 10 # Todos los textos se escriben 10 pixeles a la derecha respecto al borde de la caja
+    
+    data = familiar.find("./dataFamiliar")
+    nacimiento = data.find("./nacimiento")
+    fallecimiento = data.find("./fallecimiento")
+
+    nombre_apellido = familiar.attrib.get('nombre') + " " + familiar.attrib.get('apellido')
+    
+    lugar_nacimiento = nacimiento.find("./lugar").text
+    fecha_nacimiento = nacimiento.find("./fecha").text
+    coordenadas_nacimiento = "Puto cueva"
+
+    if fallecimiento != None:
+        lugar_fallecimiento = fallecimiento.find("./lugar").text
+        fecha_fallecimiento = fallecimiento.find("./fecha").text
+        coordenadas_fallecimiento = "Puto cueva"
+    else:
+        lugar_fallecimiento = " - "
+        fecha_fallecimiento = " - "
+        coordenadas_fallecimiento = " - "
+
+
+    write_familiar_name(nombre_apellido, x, y + 10)
+    write_text("Lugar de nacimiento: " + lugar_nacimiento, x, y + 20)
+    write_text("fecha de nacimiento: " + fecha_nacimiento, x, y + 30)
+    write_text("c. de nacimiento: " + coordenadas_nacimiento, x, y + 40)
+
+    write_text("Lugar de fallecimiento: " + lugar_fallecimiento, x, y + 50)
+    write_text("fecha de fallecimiento: " + fecha_fallecimiento, x, y + 60)
+    write_text("c. de fallecimiento: " + coordenadas_fallecimiento, x, y + 70)
+
+
+def write_familiar_name(nombre_apellidos, x, y):
+    """
+    Funcion dedicada a escribir los nombres de los familiares en el svg
+    Incluye un elemento <text>  con el siguiente formato
+        <text x="<x>" y="<y>" font-size="10" style="fill:blue"> <nombre_apellidos> </text>
+    """
+    write_in_file('<text x="{0}" y="{1}" font-size="10" style="fill:blue"> {2} </text>'.format(
+        str(x), str(y), nombre_apellidos
+    ))
+
+
+def write_text(text, x, y):
+    """
+    Incluye un elemento <text>  con el siguiente formato
+        <text x="<x>" y="<y>" font-size="8" style="fill:blue"> <paramenter_text> </paramenter_text>
+    """
+    write_in_file('<text x="{0}" y="{1}" font-size="8" style="fill:black"> {2}</text>'.format(
+        str(x), str(y), text
+    ))
+
+
+def write_in_file_svg_head():
+    """
+    Incluye la cabecera del archivo svg:
+        <?xml version="1.0" encoding="utf-8"?>
+        <svg width="auto" height="3420" style="overflow:visible " version="1.1" xmlns="http://www.w3.org/2000/svg">
+    """
+    write_in_file('<?xml version="1.0" encoding="UTF-8"?>')
+    write_in_file('<svg width="auto" height="3420" style="overflow:visible " version="1.1" xmlns="http://www.w3.org/2000/svg">')
+
+
+def main():
+    print(xml_2_svg.__doc__)
+    
+    archivo_XML = "arbolGenealogico.xml"
+    xml_2_svg(archivo_XML)
+
+    file.close()
+    
+    print("Archivo XML convertido con exito!\n")
+
+
+if __name__ == "__main__":
+    main()
